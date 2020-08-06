a76fa532bd4943a166b46f0ed4e4be1dfaf5a2c8
Sundaram Subramanian <learnwithsundar@gmail.com>
2020-08-06 10:55:33 +0530

    Initail Commit

---
+++ ErrorLogManagment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ErrorLogManagment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
